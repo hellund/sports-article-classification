39c68cf013a1209f93b3a09c7849300f408771d4
hellund <eirik.helland@hotmail.com>
2023-02-24 10:31:59 +0100

    Added new directory from which to retrieve data

--- src/data/active_learning_data.py
+++ src/data/active_learning_data.py
@@ -3,13 +3,15 @@ from src.utils import get_project_root
 
 
 def get_data():
-    """Retrieves data from directory ment for the active learning model.
+    """Retrieves data from directory meant for the active learning model.
 
     Returns:
         pd.Dataframe: Dataframe containing text and label
     """
     root = get_project_root()
-    data = pd.read_csv(root + '/src/data/active_learning_VG_data.csv',
+    data = pd.read_csv(root +
+                       '/src/data/annotation_dataset/'
+                       'vg_article_annotated_dataset.csv',
                        encoding='utf-8-sig')
     train = data.loc[:, ['text', 'label']]
     return train
@@ -17,4 +19,3 @@ def get_data():
 
 if __name__ == '__main__':
     pass
-
