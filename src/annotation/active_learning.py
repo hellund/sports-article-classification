2c53becd10a5435b8a1fdd5d12b83af344bf5785
hellund <eirik.helland@hotmail.com>
2023-02-24 10:27:40 +0100

    Fixed preprocessing and vetorization in modelling pipeline

--- src/annotation/active_learning.py
+++ src/annotation/active_learning.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from flask import Flask, request
 from src.data.preprocessing import DataPreprocessorHelland
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 import pickle
 import os
@@ -10,9 +9,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.multiclass import OneVsRestClassifier
 from src.annotation.doccano import get_latest_annotated_data
-
+from skmultilearn.problem_transform import BinaryRelevance
+import scipy
+from skmultilearn.model_selection import iterative_train_test_split
 
 app = Flask(__name__)
 
@@ -20,7 +20,9 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def predict():
     """Flask api that retrieves an input from doccano and predicts the
-    correct label using the largest active learning model.
+    correct label using the largest active learning model and chosen
+    preprocessing. The multi label binarizer is fetched and used to output
+    labels as strings.
 
     Returns:
         dict: Dict with "label" as key and predicted label as value
@@ -33,25 +35,31 @@ def predict():
     loaded_mlb = pickle.load(open('active_learning_models\\' + 'mlb_' +
                                   find_newest_model(), 'rb'))
     preprocessor = DataPreprocessorHelland(pd.Series(new_input))
+    preprocessor.preprocess()
     processed_input = preprocessor.text_series
     pred_labels = loaded_model.predict([processed_input['text']])
     pred_labels = loaded_mlb.inverse_transform(pred_labels)
 
     pred_proba = loaded_model.predict_proba([processed_input['text']])
-    prob_dict = {k: v for k, v in zip(loaded_mlb.classes_, pred_proba[0])}
+    prob_dict = {k: v for k, v in
+                 zip(loaded_mlb.classes_, pred_proba.toarray()[0])}
 
     print('Predicted labels:\n' + f'{pred_labels[0]}\n')
-    print(f'Probabilities:\n{prob_dict}\n')
-
+    print(f'Probabilities:')
+    for key in prob_dict.keys():
+        print(f'{key}: {prob_dict[key]}')
+    print('\n')
     return {'label': pred_labels[0]}
 
 
-
 def train_SVC_multilabel_model(X, y, C, gamma):
-    """Trains a SVC multi label model using inputted data and parameters and
-    pickles the resulting model. The pickled model is saved in the
-    active_learning_models directory with with a multilabelbinarizer object
-    for mapping the model predictions back to strings.
+    """Trains a Binary Relevance SVC multi label model using inputted data
+    and parameters and pickles the resulting model. The pickled model is
+    saved in the active_learning_models directory with with a
+    multilabelbinarizer object for mapping the model predictions back to
+    strings. The inputted data is vectorized and inverse vectorized for the
+    iterative_train_test_split from skmultilearn to work and to be compatible
+    with new texts.
 
     Args:
         X (pd.Series): pd.Series with text data
@@ -66,42 +74,54 @@ def train_SVC_multilabel_model(X, y, C, gamma):
     mlb.fit(y)
     y = mlb.transform(y)
     y = pd.DataFrame(y, columns=mlb.classes_)
-    vectorizer = TfidfVectorizer()
-    x_train, x_test, y_train, y_test = train_test_split(X,
-                                                        y,
-                                                        test_size=0.2,
-                                                        random_state=0)
 
-    model = OneVsRestClassifier(SVC(kernel='rbf', probability=True, C=C,
-                                    gamma=gamma))
+    vectorizer = TfidfVectorizer()
+    X_vectorized = vectorizer.fit_transform(X)
+    X = vectorizer.inverse_transform(X_vectorized)
+    X = pd.Series(X).apply(' '.join)
+
+    y = scipy.sparse.csr_matrix(y.values)
+
+    x_train, y_train, x_test, y_test = iterative_train_test_split(X_vectorized,
+                                                                  y,
+                                                                  test_size=0.3)
+
+    x_train = vectorizer.inverse_transform(x_train)
+    x_test = vectorizer.inverse_transform(x_test)
+
+    x_train = pd.Series(x_train).apply(' '.join)
+    x_test = pd.Series(x_test).apply(' '.join)
+
+    model = BinaryRelevance(classifier=SVC(kernel='rbf', probability=True, C=C,
+                                           gamma=gamma),
+                            require_dense=[False, True])
 
     pipeline = make_pipeline(vectorizer, model)
     model = pipeline.fit(x_train, y_train)
 
-    filename = f'active_learning_ml_model_{X.shape[0]}.pkl'
-    pickle.dump(model, open('active_learning_models\\' + filename, 'wb'))
-    pickle.dump(mlb, open('active_learning_models\\' + 'mlb_' + filename, 'wb'))
-    # Predict class labels on training data
     pred_labels_tr = model.predict(x_train)
-    # Predict class labels on a test data
     pred_labels_te = model.predict(x_test)
 
-    print(mlb.inverse_transform(pred_labels_tr))
 
-    # Use score method to get accuracy of the model
     print('----- Evaluation on Test Data -----')
     score_te = model.score(x_test, y_test)
     print('Accuracy Score: ', score_te)
-    # Look at classification report to evaluate the model
-    print(classification_report(y_test, pred_labels_te))
+    print(classification_report(y_test, pred_labels_te,
+                                target_names=mlb.classes_))
     print('--------------------------------------------------------')
 
     print('----- Evaluation on Training Data -----')
     score_tr = model.score(x_train, y_train)
     print('Accuracy Score: ', score_tr)
-    # Look at classification report to evaluate the model
-    print(classification_report(y_train, pred_labels_tr))
+    print(classification_report(y_train, pred_labels_tr,
+                                target_names=mlb.classes_))
     print('--------------------------------------------------------')
+
+    model = pipeline.fit(X, y)
+
+    filename = f'active_learning_ml_model_{X.shape[0]}.pkl'
+    pickle.dump(model, open('active_learning_models\\' + filename, 'wb'))
+    pickle.dump(mlb, open('active_learning_models\\' + 'mlb_' + filename, 'wb'))
 
 
 def find_newest_model():
@@ -146,7 +166,6 @@ def run_train_ml_model():
 
 
 def main():
-    run_train_ml_model()
     """Trains a new SVC multi label model with the latest dataset from 
     doccano annotation and saves it to active_learning_models if __name__ == 
     "__main__".
@@ -155,8 +174,8 @@ def main():
         NoneType
 
     """
+    run_train_ml_model()
 
 
-# if __name__ == '__main__':
-#     #main()
-
+if __name__ == '__main__':
+    main()
